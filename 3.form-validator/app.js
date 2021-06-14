346bca5017dd4c91ce2a4b055ec8f7842a42d159
mhdhfz <mhd.hfzz@gmail.com>
2021-06-14 14:42:20 +0800

    add checker for username,password and email

--- 3.form-validator/app.js
+++ 3.form-validator/app.js
@@ -16,9 +16,19 @@ function showSuccess(input) {
     formControl.className = 'form-control success';
 }
 
-function isEmailValid(email) {
+function checkEmail(input) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Email is not valid');
+    }
+}
+
+function checkPasswordMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, 'Password do not match')
+    }
 }
 
 function getFieldName(input) {
@@ -35,10 +45,24 @@ function checkRequired(inputArr) {
     });
 }
 
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    } else {
+        showSuccess(input)
+    }
+}
+
 function registerForm(e) {
     e.preventDefault();
 
     checkRequired([usernameInput,emailInput,passwordInput,confirmPasswordInput])
+    checkLength(username, 3 , 15);
+    checkLength(password, 6 , 25);
+    checkEmail(email);
+    checkPasswordMatch(passwordInput, confirmPasswordInput);
 }
 
 form.addEventListener('submit', registerForm);
